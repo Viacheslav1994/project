4695785f87825dbc5396863038f701f5e1a73888
Slava <klechkovskyy1994@gmail.com>
2020-05-15 16:24:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strick";
+let sss = 5;
+sss = 10;
+console.log(sss);
+
+//const result = confirm("are you here7");
+//console.log(result);
+
+
+//const user = "Ivan";
+//alert(`привет,${user}`);
+
+let incr = 10, decr = 10;
+incr++;
+decr--;
+
+console.log(incr++);
